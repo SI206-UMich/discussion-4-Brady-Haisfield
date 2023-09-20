b663f7df177c309f85481b60c076c67d25e8570f
Braden <braden@Bradens-MacBook-Pro-3.local>
2023-09-20 12:41:50 -0400

    Discussion Practice

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -7,6 +7,10 @@ class Rectangle():
 
     # YOUR CODE HERE
 
+    def __init__(self, width, height):
+        self.width=width
+        self.height=height
+
 
 
     # Create the "__str__" method
@@ -15,6 +19,8 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
 
@@ -25,6 +31,11 @@ class Rectangle():
     #       False otherwise
 
     # YOUR CODE HERE
+    def verify_input(self):
+        if self.width> 0 and self.height>0:
+            return True
+        else:
+            return False
 
 
 
@@ -34,6 +45,14 @@ class Rectangle():
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
+    def area(self):
+        if self.verify_input()== False:
+            return 'Invalid input'
+        else:
+            return self.width*self.height
+
+
+
 
 
 
@@ -43,7 +62,13 @@ class Rectangle():
     # Otherwise, it returns the perimeter of the rectangle.
 
     # YOUR CODE HERE
-    
+
+    def perimeter(self):
+        if self.verify_input()== False:
+            return 'Invalid input'
+        else:
+            return (self.width*2)+(self.height*2)
+
 
 
 def main():
